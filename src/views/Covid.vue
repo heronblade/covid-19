d7f82af7439ab0cc5d7e3c5ffd8f06d08a23b6c3
heronblade <bylerj@gmail.com>
2020-03-20 17:59:54 -0400

    forgot how much i can't stand the bad documentation of d3 v3 vs v5

---
+++ src/views/Covid.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="covid">
+    <p>Covid Page</p>
+    <choropleth
+      title="Plan Enrollment Share by County"
+      :state="state"></choropleth>
+  </div>
+</template>
+
+<script>
+import choropleth from '../components/choropleth.vue';
+
+export default {
+  name: 'covid',
+  data() {
+    return {
+      state: 'Pennsylvania',
+    };
+  },
+  components: {
+    choropleth,
+  },
+};
+</script>
