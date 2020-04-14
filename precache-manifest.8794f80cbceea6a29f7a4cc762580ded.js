69151da90e9915768781fdc83f42ebf03a0dd923
heronblade <bylerj@gmail.com>
2020-04-14 14:53:54 -0400

    deploy

---
+++ precache-manifest.8794f80cbceea6a29f7a4cc762580ded.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c368570dfe3e7aaf285001c1e626c3fe",
+    "url": "/covid-19/bls-population-estimate.json"
+  },
+  {
+    "revision": "f0ac1c34d41a5d9658aa",
+    "url": "/covid-19/css/app.dc4a49c0.css"
+  },
+  {
+    "revision": "535877f50039c0cb49a6196a5b7517cd",
+    "url": "/covid-19/fonts/element-icons.535877f5.woff"
+  },
+  {
+    "revision": "732389ded34cb9c52dd88271f1345af9",
+    "url": "/covid-19/fonts/element-icons.732389de.ttf"
+  },
+  {
+    "revision": "623b31b25bdc621bb58d854fda83aeae",
+    "url": "/covid-19/index.html"
+  },
+  {
+    "revision": "ecde0963b831288a1314",
+    "url": "/covid-19/js/about.d83ed877.js"
+  },
+  {
+    "revision": "f0ac1c34d41a5d9658aa",
+    "url": "/covid-19/js/app.60d848b6.js"
+  },
+  {
+    "revision": "542e17c81bbe9295e659",
+    "url": "/covid-19/js/chunk-vendors.aed5082a.js"
+  },
+  {
+    "revision": "799586e693cb4d66ecf51c4b5af65fe9",
+    "url": "/covid-19/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/covid-19/robots.txt"
+  },
+  {
+    "revision": "340049236779792eed359bb42f2e52e6",
+    "url": "/covid-19/test.html"
+  },
+  {
+    "revision": "570c0fc991155e30493b3bfae02efd79",
+    "url": "/covid-19/us-albers-counties.json"
+  },
+  {
+    "revision": "62990fb872606bfbe90d8f95ad7dbb34",
+    "url": "/covid-19/us-albers.json"
+  }
+]);
